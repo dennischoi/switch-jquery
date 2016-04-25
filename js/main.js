09739ebe9cf6f20776dd20042ed2cc46536625a5
Dennis Choi <dennis.choi7@gmail.com>
2016-04-25 17:45:38 -0400

    jQuery to have on and off function of button, through class. Swap background color through the same method, and change the .html text depending on the button class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+$(document).on('ready', function() {
+  alert('READY')
+
+  $("button").on('click', function() {
+    if ($("button").hasClass('on')){
+      $("button").removeClass('on').addClass('off');
+      $("body").removeClass('dark').addClass('light');
+      $(".status").html("It's so bright in here!")
+    }else if ($("button").hasClass('off')){
+      $("button").removeClass('off').addClass('on');
+      $("body").removeClass('light').addClass('dark');
+      $(".status").html("It's so dark in here!")
+    }
+  });
 
 
+
+});
